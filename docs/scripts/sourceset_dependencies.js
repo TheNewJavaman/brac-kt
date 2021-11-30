432e581859f799ca5e1fef8d7deebbe3398c4575
Gabriel Pizarro <gpizarro@javaman.net>
2021-11-30 10:33:30 -0800

    Remove docs from main branch ❌

--- docs/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":ibmq-provider:dokkaHtmlPartial/commonMain":[],":ibmq-provider:dokkaHtmlPartial/jvmMain":[":ibmq-provider:dokkaHtmlPartial/commonMain"],":api:dokkaHtmlPartial/commonMain":[],":api:dokkaHtmlPartial/jvmMain":[":api:dokkaHtmlPartial/commonMain"]}'
